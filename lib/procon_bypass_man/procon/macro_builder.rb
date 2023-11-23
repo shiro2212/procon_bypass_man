e735e85ab2d3527dd0e74dd45b0305bc83455c02
Your Name <you@example.com>
2023-11-23 22:56:28 +0900

    macro調整

--- lib/procon_bypass_man/procon/macro_builder.rb
+++ lib/procon_bypass_man/procon/macro_builder.rb
@@ -107,9 +107,9 @@ class ProconBypassMan::Procon::MacroBuilder
 
     if /^forward_ikarole_r/ =~ step
       for_forward_ikarole_steps = [
-        [:tilt_left_stick_completely_to_30deg],
-        [:tilt_left_stick_completely_to_30deg],
-        [:tilt_left_stick_completely_to_30deg],
+        [:tilt_left_stick_completely_to_0deg],
+        [:tilt_left_stick_completely_to_0deg],
+        [:tilt_left_stick_completely_to_0deg],
         [:tilt_left_stick_completely_to_90deg, :b],
       ]
       return { steps: for_forward_ikarole_steps }

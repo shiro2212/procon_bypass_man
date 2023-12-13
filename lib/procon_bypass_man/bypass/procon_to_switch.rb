ff1bce6c690a60335b1ae4bc33c66f46b6b29ec9
Your Name <you@example.com>
2023-12-14 00:24:31 +0900

    debug

--- lib/procon_bypass_man/bypass/procon_to_switch.rb
+++ lib/procon_bypass_man/bypass/procon_to_switch.rb
@@ -65,8 +65,10 @@ class ProconBypassMan::Bypass::ProconToSwitch
         end
 
         gyro = nil
+        ProconBypassMan.logger.debug { "[ExternalInput] gyro0"}
         if !(external_input_data.nil?)
-          if external_input_data.buttons.include?(":unzr")
+          ProconBypassMan.logger.debug { "[ExternalInput] nil?"}
+          if external_input_data.raw_data.include?(":unzr")
             gyro = BlueGreenProcess::SharedVariable.instance.data["recent_gyro"][7]
             ProconBypassMan.logger.debug { "[ExternalInput] gyro:#{gyro}"}
           end

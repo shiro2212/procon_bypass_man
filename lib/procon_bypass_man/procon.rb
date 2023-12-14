2376b627f8131e4dd857a65e5ec5f6cac6978b63
Your Name <you@example.com>
2023-12-14 23:09:30 +0900

    debug

--- lib/procon_bypass_man/procon.rb
+++ lib/procon_bypass_man/procon.rb
@@ -106,6 +106,7 @@ class ProconBypassMan::Procon
     analog_stick = ProconBypassMan::Procon::AnalogStick.new(binary: user_operation.binary.raw)
     add_recent_left_stick_hypotenuses(analog_stick.relative_hypotenuse)
     dumped_tilting_power = @left_stick_tilting_power_scaler.calculate(recent_left_stick_hypotenuses)
+    user_operation.binary.raw[13..48].to_json
     # add_recent_gyro(user_operation.binary.raw[13..48])
 
     enable_all_macro = true

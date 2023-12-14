a35eb9b758c7c3997d3cc727920faef6dde08366
Your Name <you@example.com>
2023-12-15 00:38:05 +0900

    debug

--- lib/procon_bypass_man/procon/value_objects/gyro.rb
+++ lib/procon_bypass_man/procon/value_objects/gyro.rb
@@ -3,6 +3,8 @@
 class ProconBypassMan::Procon::Gyro
   
     def initialize(binary: )
+        bytes13 = binary[13].unpack("S*")
+        bytes14 = binary[14].unpack("S*")
         # bytes13 = binary[13] & ((1 << 8) - 1)
         # bytes14 = binary[14] & ((1 << 8) - 1)
         # uint16le = (bytes14 << 8) | bytes13
@@ -10,7 +12,7 @@ class ProconBypassMan::Procon::Gyro
         # if uint16le >= 32768
         #     int16le = uint16le - 65536
         # ProconBypassMan.logger.debug {"[gyro.rb] int16le:#{int16le}"}
-        ProconBypassMan.logger.debug {"[gyro.rb] int16le:"}
+        ProconBypassMan.logger.debug {"[gyro.rb] int16le:#{bytes13}, #{bytes14}"}
         freeze
     end
   

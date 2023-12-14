54ca1a3111328c1ac9b1fa8cf2c7f2704d4dd61b
Your Name <you@example.com>
2023-12-15 00:12:17 +0900

    debug

--- lib/procon_bypass_man/procon/value_objects/gyro.rb
+++ lib/procon_bypass_man/procon/value_objects/gyro.rb
@@ -6,8 +6,9 @@ class ProconBypassMan::Procon::Gyro
         bytes13 = binary[13] & ((1 << 8) - 1)
         bytes14 = binary[14] & ((1 << 8) - 1)
         uint16le = (bytes14 << 8) | bytes13
-        int16le = uint16le if uint16le < 32768 else uint16le - 65536
-        # byte13, byte14 = bytes.each_char.map { |x| x.unpack("I").first.to_s(2).rjust(8, "0") }
+        int16le = uint16le
+        if uint16le >= 32768
+            int16le = uint16le - 65536
         ProconBypassMan.logger.debug {"[gyro.rb] int16le:#{int16le}"}
         freeze
     end

0ffd89bb24a6545eb3ab6fdc10eaaea9b7012096
Your Name <you@example.com>
2023-12-16 01:04:29 +0900

    debug

--- lib/procon_bypass_man/procon/value_objects/gyro.rb
+++ lib/procon_bypass_man/procon/value_objects/gyro.rb
@@ -18,9 +18,8 @@ class ProconBypassMan::Procon::Gyro
         @gyro_2 = bin_gyro_2 < 32768 ? bin_gyro_2 : bin_gyro_2 - 65536
         @gyro_3 = bin_gyro_3 < 32768 ? bin_gyro_3 : bin_gyro_3 - 65536
         # S< # [gyro.rb] accel_x:[64781], accel_y:[65514], accel_z:[4076],, gyro_1:[65525], gyro_2:[65525], gyro_3:[3]
-        # S> # [gyro.rb] accel_x:[3581], accel_y:[60415], accel_z:[59919],, gyro_1:[62719], gyro_2:[63487], gyro_3:[512]
-        # S* # [gyro.rb] accel_x:[64780], accel_y:[65521], accel_z:[4088],, gyro_1:[65524], gyro_2:[65528], gyro_3:[4]
-        ProconBypassMan.logger.debug {"[gyro.rb] accel_x:#{accel_x}, accel_y:#{accel_y}, accel_z:#{accel_z},, gyro_1:#{gyro_1}, gyro_2:#{gyro_2}, gyro_3:#{gyro_3}"}
+        # int# [gyro.rb] accel_x:-752, accel_y:-22, accel_z:4073,, gyro_1:-11, gyro_2:-9, gyro_3:3
+        # ProconBypassMan.logger.debug {"[gyro.rb] accel_x:#{accel_x}, accel_y:#{accel_y}, accel_z:#{accel_z},, gyro_1:#{gyro_1}, gyro_2:#{gyro_2}, gyro_3:#{gyro_3}"}
         freeze
     end
     

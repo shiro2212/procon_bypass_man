96a899a67a6862475aeca83bebfb1fb9146ba41e
Your Name <you@example.com>
2023-12-16 00:57:56 +0900

    debug

--- lib/procon_bypass_man/procon/value_objects/gyro.rb
+++ lib/procon_bypass_man/procon/value_objects/gyro.rb
@@ -4,12 +4,19 @@ class ProconBypassMan::Procon::Gyro
     attr_accessor :accel_x, :accel_y, :accel_z, :gyro_1, :gyro_2, :gyro_3
   
     def initialize(binary: )
-        @accel_x = binary[13..14].unpack("S<").first < 32768 ? binary[13..14].unpack("S<").first : binary[13..14].unpack("S<").first - 65536
-        @accel_y = binary[15..16].unpack("S<").first < 32768 ? binary[15..16].unpack("S<").first : binary[15..16].unpack("S<").first - 65536
-        @accel_z = binary[17..18].unpack("S<").first < 32768 ? binary[17..18].unpack("S<").first : binary[17..18].unpack("S<").first - 65536
-        @gyro_1 = binary[19..20].unpack("S<").first < 32768 ? binary[19..20].unpack("S<").first : binary[19..20].unpack("S<").first - 65536
-        @gyro_2 = binary[21..22].unpack("S<").first < 32768 ? binary[21..22].unpack("S<").first : binary[21..22].unpack("S<").first - 65536
-        @gyro_3 = binary[23..24].unpack("S<").first < 32768 ? binary[23..24].unpack("S<").first : binary[23..24].unpack("S<").first - 65536
+        bin_accel_x = binary[13..14].unpack("S<").first
+        bin_accel_y = binary[15..16].unpack("S<").first
+        bin_accel_z = binary[17..18].unpack("S<").first
+        @accel_x = bin_accel_x < 32768 ? bin_accel_x : bin_accel_x - 65536
+        @accel_y = bin_accel_y < 32768 ? bin_accel_y : bin_accel_y - 65536
+        @accel_z = bin_accel_z < 32768 ? bin_accel_z : bin_accel_z - 65536
+
+        bin_gyro_1 = binary[19..20].unpack("S<").first
+        bin_gyro_2 = binary[21..22].unpack("S<").first
+        bin_gyro_3 = binary[23..24].unpack("S<").first
+        @gyro_1 = bin_gyro_1 < 32768 ? bin_gyro_1 : bin_gyro_1 - 65536
+        @gyro_2 = bin_gyro_2 < 32768 ? bin_gyro_2 : bin_gyro_2 - 65536
+        @gyro_3 = bin_gyro_3 < 32768 ? bin_gyro_3 : bin_gyro_3 - 65536
         # S< # [gyro.rb] accel_x:[64781], accel_y:[65514], accel_z:[4076],, gyro_1:[65525], gyro_2:[65525], gyro_3:[3]
         # S> # [gyro.rb] accel_x:[3581], accel_y:[60415], accel_z:[59919],, gyro_1:[62719], gyro_2:[63487], gyro_3:[512]
         # S* # [gyro.rb] accel_x:[64780], accel_y:[65521], accel_z:[4088],, gyro_1:[65524], gyro_2:[65528], gyro_3:[4]

58a39cf432efca8f8e1643494162cca25869bad7
Your Name <you@example.com>
2023-12-16 00:08:11 +0900

    debug

--- lib/procon_bypass_man/procon.rb
+++ lib/procon_bypass_man/procon.rb
@@ -270,7 +270,7 @@ class ProconBypassMan::Procon
       if gyro
         self.user_operation.apply_gyro(gyro:gyro)
       end
-      ProconBypassMan.logger.debug {"[procon] bin[13]: #{self.user_operation.binary[13]}"}
+      ProconBypassMan.logger.debug {"[procon] bin[14]: #{self.user_operation.binary[14]}"}
       return self.user_operation.binary.raw
     end
 

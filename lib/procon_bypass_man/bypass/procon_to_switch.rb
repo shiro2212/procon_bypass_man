88dfdf2216d578b81152612d55c5aa2e461682bb
Your Name <you@example.com>
2023-12-16 01:31:10 +0900

    commit

--- lib/procon_bypass_man/bypass/procon_to_switch.rb
+++ lib/procon_bypass_man/bypass/procon_to_switch.rb
@@ -68,7 +68,7 @@ class ProconBypassMan::Bypass::ProconToSwitch
         if !(external_input_data.nil?)
           if external_input_data.raw_data.include?(":unzr")
             gyro = []
-            frame = 0
+            frame = 10
             gyro <<  BlueGreenProcess::SharedVariable.instance.data["recent_accel_x"][frame].pack("S*<")
             gyro <<  BlueGreenProcess::SharedVariable.instance.data["recent_accel_y"][frame].pack("S*<")
             gyro <<  BlueGreenProcess::SharedVariable.instance.data["recent_accel_z"][frame].pack("S*<")

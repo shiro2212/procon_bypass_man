a585d5fe2115b79cf16014a4712a69d34dfb8a06
Your Name <you@example.com>
2023-12-16 00:52:16 +0900

    debug

--- lib/procon_bypass_man/procon/value_objects/gyro.rb
+++ lib/procon_bypass_man/procon/value_objects/gyro.rb
@@ -4,16 +4,12 @@ class ProconBypassMan::Procon::Gyro
     attr_accessor :accel_x, :accel_y, :accel_z, :gyro_1, :gyro_2, :gyro_3
   
     def initialize(binary: )
-        hByteBe = binary[13].unpack("C").first
-        lByteBe = binary[14].unpack("C").first
-        uint16 = (lByteBe << 8) | hByteBe
-
-        @accel_x = binary[13..14].unpack("S<")
-        @accel_y = binary[15..16].unpack("S<")
-        @accel_z = binary[17..18].unpack("S<")
-        @gyro_1 = binary[19..20].unpack("S<")
-        @gyro_2 = binary[21..22].unpack("S<")
-        @gyro_3 = binary[23..24].unpack("S<")
+        @accel_x = binary[13..14].unpack("S<").first < 32768 ? binary[13..14].unpack("S<").first : binary[13..14].unpack("S<").first - 65536
+        @accel_y = binary[15..16].unpack("S<").first < 32768 ? binary[15..16].unpack("S<").first : binary[15..16].unpack("S<").first - 65536
+        @accel_z = binary[17..18].unpack("S<").first < 32768 ? binary[17..18].unpack("S<").first : binary[17..18].unpack("S<").first - 65536
+        @gyro_1 = binary[19..20].unpack("S<").first < 32768 ? binary[19..20].unpack("S<").first : binary[19..20].unpack("S<").first - 65536
+        @gyro_2 = binary[21..22].unpack("S<").first < 32768 ? binary[21..22].unpack("S<").first : binary[21..22].unpack("S<").first - 65536
+        @gyro_3 = binary[23..24].unpack("S<").first < 32768 ? binary[23..24].unpack("S<").first : binary[23..24].unpack("S<").first - 65536
         # S< # [gyro.rb] accel_x:[64781], accel_y:[65514], accel_z:[4076],, gyro_1:[65525], gyro_2:[65525], gyro_3:[3]
         # S> # [gyro.rb] accel_x:[3581], accel_y:[60415], accel_z:[59919],, gyro_1:[62719], gyro_2:[63487], gyro_3:[512]
         # S* # [gyro.rb] accel_x:[64780], accel_y:[65521], accel_z:[4088],, gyro_1:[65524], gyro_2:[65528], gyro_3:[4]

48cd8b17512a27094cc324e9f27ca8780ee52a16
Your Name <you@example.com>
2023-12-15 13:37:47 +0900

    debug

--- lib/procon_bypass_man/procon/value_objects/gyro.rb
+++ lib/procon_bypass_man/procon/value_objects/gyro.rb
@@ -14,7 +14,7 @@ class ProconBypassMan::Procon::Gyro
         accel_z = (bytes18 << 8) | bytes17
         bytes19 = binary[19].unpack("C")
         bytes20 = binary[20].unpack("C")
-        gyro_1 = (bytes16 << 8) | bytes15
+        gyro_1 = (bytes20 << 8) | bytes19
         
         bytes21 = binary[21].unpack("C")
         bytes22 = binary[22].unpack("C")
@@ -23,6 +23,14 @@ class ProconBypassMan::Procon::Gyro
         bytes23 = binary[23].unpack("C")
         bytes24 = binary[24].unpack("C")
         gyro_3 = (bytes24 << 8) | bytes23
+
+        accel_x_s = binary[13..14].unpack("S")
+        accel_y_s = binary[15..16].unpack("S")
+        accel_z_s = binary[17..18].unpack("S")
+        gyro_1_s = binary[19..20].unpack("S")
+        gyro_2_s = binary[21..22].unpack("S")
+        gyro_3_s = binary[23..24].unpack("S")
+
         # bytes13 = binary[13] >> 0 & ((1 << 8) - 1)
         # bytes14 = binary[14] >> 0 & ((1 << 8) - 1)
         # uint16le = (bytes14 << 8) | bytes13
@@ -31,7 +39,7 @@ class ProconBypassMan::Procon::Gyro
         #     int16le = uint16le - 65536
         # ProconBypassMan.logger.debug {"[gyro.rb] int16le:#{int16le}"}
         ProconBypassMan.logger.debug {"[gyro.rb] accel_x:#{accel_x}, accel_y:#{accel_y}, accel_z:#{accel_z},, gyro_1:#{gyro_1}, gyro_2:#{gyro_2}, gyro_3:#{gyro_3}"}
-
+        ProconBypassMan.logger.debug {"[gyro.rb] accel_x_s:#{accel_x_s}, accel_y_s:#{accel_y_s}, accel_z_s:#{accel_z_s},, gyro_1_s:#{gyro_1_s}, gyro_2_s:#{gyro_2_s}, gyro_3_s:#{gyro_3_s}"}
         freeze
     end
   

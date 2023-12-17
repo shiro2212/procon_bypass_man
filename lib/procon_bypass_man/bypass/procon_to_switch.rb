d0179bf97ff359c9a47c0e93ba8a3ba619a872de
Your Name <you@example.com>
2023-12-17 15:00:34 +0900

    debug

--- lib/procon_bypass_man/bypass/procon_to_switch.rb
+++ lib/procon_bypass_man/bypass/procon_to_switch.rb
@@ -69,47 +69,46 @@ class ProconBypassMan::Bypass::ProconToSwitch
           if external_input_data.raw_data.include?("_aim_")
             gyro = []
             frame = 0
-            # accelX = 0
-            # for i in 0..6
-            #   accelX += BlueGreenProcess::SharedVariable.instance.data["recent_accel_x"][i].first
-            # end
-            # gyro <<  [accelX * -1].pack("S*<")
-            # accelY = 0
-            # for i in 0..6
-            #   accelY += BlueGreenProcess::SharedVariable.instance.data["recent_accel_y"][i].first
-            # end
-            # gyro <<  [accelY * -1].pack("S*<")
-            # accelZ = 0
-            # for i in 0..6
-            #   accelZ += BlueGreenProcess::SharedVariable.instance.data["recent_accel_z"][i].first
-            # end
-            # gyro <<  [accelZ * -1].pack("S*<")
+            accelX = 0
+            for i in 0..6
+              accelX += BlueGreenProcess::SharedVariable.instance.data["recent_accel_x"][i].first
+            end
+            gyro <<  [accelX * -1].pack("S*<")
+            accelY = 0
+            for i in 0..6
+              accelY += BlueGreenProcess::SharedVariable.instance.data["recent_accel_y"][i].first
+            end
+            gyro <<  [accelY * -1].pack("S*<")
+            accelZ = 0
+            for i in 0..6
+              accelZ += BlueGreenProcess::SharedVariable.instance.data["recent_accel_z"][i].first
+            end
+            gyro <<  [accelZ * -1].pack("S*<")
 
-            # gyro1 = 0
-            # for i in 0..6
-            #   gyro1 += BlueGreenProcess::SharedVariable.instance.data["recent_gyro_1"][i].first
-            # end
-            # gyro <<  [gyro1 * -1].pack("S*<")
-            # gyro2 = 0
-            # for i in 0..6
-            #   gyro2 += BlueGreenProcess::SharedVariable.instance.data["recent_gyro_2"][i].first
-            # end
-            # gyro <<  [gyro2 * -1].pack("S*<")
-            # gyro3 = 0
-            # for i in 0..6
-            #   gyro3 += BlueGreenProcess::SharedVariable.instance.data["recent_gyro_3"][i].first
-            # end
-            # # gyro <<  [gyro3 * -1].pack("S*<")
-            # gyro <<  ([BlueGreenProcess::SharedVariable.instance.data["recent_accel_z"][frame].first * -1]).pack("S*<")
+            gyro1 = 0
+            for i in 0..6
+              gyro1 += BlueGreenProcess::SharedVariable.instance.data["recent_gyro_1"][i].first
+            end
+            gyro <<  [gyro1 * -1].pack("S*<")
+            gyro2 = 0
+            for i in 0..6
+              gyro2 += BlueGreenProcess::SharedVariable.instance.data["recent_gyro_2"][i].first
+            end
+            gyro <<  [gyro2 * -1].pack("S*<")
+            gyro3 = 0
+            for i in 0..6
+              gyro3 += BlueGreenProcess::SharedVariable.instance.data["recent_gyro_3"][i].first
+            end
+            # gyro <<  [gyro3 * -1].pack("S*<")
             # gyro <<  ([BlueGreenProcess::SharedVariable.instance.data["recent_gyro_1"][frame].first * -1]).pack("S*<")
             # gyro <<  ([BlueGreenProcess::SharedVariable.instance.data["recent_gyro_2"][frame].first * -1]).pack("S*<")
             # gyro <<  ([BlueGreenProcess::SharedVariable.instance.data["recent_gyro_3"][frame].first * -1]).pack("S*<")
-            gyro << [32767].pack("S*<")
-            gyro << [32767].pack("S*<")
-            gyro << [32767].pack("S*<")
-            gyro << [32767].pack("S*<")
-            gyro << [32767].pack("S*<")
-            gyro << [32767].pack("S*<")
+            # gyro << [32767].pack("S*<")
+            # gyro << [32767].pack("S*<")
+            # gyro << [32767].pack("S*<")
+            # gyro << [32767].pack("S*<")
+            # gyro << [32767].pack("S*<")
+            # gyro << [32767].pack("S*<")
             # ProconBypassMan.logger.debug {"[SharedVariable] packH: #{gyro}"}
           end
         end

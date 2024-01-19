96ab914a894244e284abea13f654042854ad76c6
Your Name <you@example.com>
2024-01-19 22:31:04 +0900

    macro

--- lib/procon_bypass_man/procon/macro_builder.rb
+++ lib/procon_bypass_man/procon/macro_builder.rb
@@ -138,6 +138,21 @@ class ProconBypassMan::Procon::MacroBuilder
       return { steps: for_forward_ikarole_steps }
     end
 
+    if /^forward_ikarole_mid/ =~ step
+      for_forward_ikarole_steps = [
+        [:tilt_left_stick_completely_to_200deg],
+        [:tilt_left_stick_completely_to_200deg],
+        [:tilt_left_stick_completely_to_200deg],
+        [:tilt_left_stick_completely_to_200deg],
+        [:tilt_left_stick_completely_to_200deg],
+        [:tilt_left_stick_completely_to_200deg],
+        [:tilt_left_stick_completely_to_200deg],
+        [:tilt_left_stick_completely_to_200deg],
+        [:tilt_left_stick_completely_to_90deg, :b],
+      ]
+      return { steps: for_forward_ikarole_steps }
+    end
+
     if /^forward_ikarole_l/ =~ step
       for_forward_ikarole_steps = [
         [:tilt_left_stick_completely_to_180deg],

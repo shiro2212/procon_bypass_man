3857e13d08e48724400d9f4c5c1fdfa3f0cbc26d
いちくにいろは <35602064+shiro2212@users.noreply.github.com>
2026-07-05 23:43:54 +0900

    Update horizontal stick tilt expectations

--- spec/lib/procon_bypass_man/procon/analog_stick_manipulator_spec.rb
+++ spec/lib/procon_bypass_man/procon/analog_stick_manipulator_spec.rb
@@ -5,14 +5,20 @@ describe ProconBypassMan::Procon::AnalogStickManipulator do
   let(:data) { "30778105800099277344e86b0a7909f4f5a8f4b500c5ff8dff6c09cdf5b8f49a00c5ff92ff6a0979f5eef46500d5ff9bff000000000000000000000000000000" }
 
   describe 'iv' do
+    before do
+      allow(ProconBypassMan::ButtonsSettingConfiguration.instance).to receive(:neutral_position) { OpenStruct.new(x: 2124, y: 1808) }
+    end
+
     it do
       manipulator = ProconBypassMan::Procon::AnalogStickManipulator.new(binary, method: :tilt_left_stick_completely_to_left)
-      expect(manipulator.manipulated_abs_x).to eq(400)
+      expect(manipulator.manipulated_abs_x).to eq(324)
+      expect(manipulator.manipulated_abs_y).to eq(1808)
     end
 
     it do
       manipulator = ProconBypassMan::Procon::AnalogStickManipulator.new(binary, method: :tilt_left_stick_completely_to_right)
-      expect(manipulator.manipulated_abs_x).to eq(3400)
+      expect(manipulator.manipulated_abs_x).to eq(3924)
+      expect(manipulator.manipulated_abs_y).to eq(1808)
     end
   end
 

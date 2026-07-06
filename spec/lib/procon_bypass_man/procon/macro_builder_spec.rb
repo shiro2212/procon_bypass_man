e91a12db330bd7be7733a04c2faebb602b5ccdb8
いちくにいろは <35602064+shiro2212@users.noreply.github.com>
2026-07-06 09:28:52 +0900

    Update dynamic ikarole eleven frame spec

--- spec/lib/procon_bypass_man/procon/macro_builder_spec.rb
+++ spec/lib/procon_bypass_man/procon/macro_builder_spec.rb
@@ -239,6 +239,8 @@ describe ProconBypassMan::Procon::MacroBuilder do
               [:tilt_left_stick_completely_to_150deg],
               [:tilt_left_stick_completely_to_150deg],
               [:tilt_left_stick_completely_to_150deg],
+              [:tilt_left_stick_completely_to_150deg],
+              [:tilt_left_stick_completely_to_150deg],
               [:tilt_left_stick_completely_to_50deg, :b],
               [:tilt_left_stick_completely_to_50deg, :b],
             ]}
@@ -248,6 +250,8 @@ describe ProconBypassMan::Procon::MacroBuilder do
         it 'normalizes negative degrees' do
           expect(described_class.new([:dynamic_ikarole], context: { left_stick_degree: 0 }).build).to eq([
             { steps: [
+              [:tilt_left_stick_completely_to_90deg],
+              [:tilt_left_stick_completely_to_90deg],
               [:tilt_left_stick_completely_to_90deg],
               [:tilt_left_stick_completely_to_90deg],
               [:tilt_left_stick_completely_to_90deg],
@@ -280,6 +284,8 @@ describe ProconBypassMan::Procon::MacroBuilder do
 
           expect(described_class.new([:dynamic_ikarole], context: { left_stick_degree: 60 }).build).to eq([
             { steps: [
+              [:tilt_left_stick_completely_to_330deg],
+              [:tilt_left_stick_completely_to_330deg],
               [:tilt_left_stick_completely_to_330deg],
               [:tilt_left_stick_completely_to_330deg],
               [:tilt_left_stick_completely_to_330deg],
